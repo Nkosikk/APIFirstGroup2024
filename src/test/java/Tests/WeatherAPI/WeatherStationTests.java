d07a79abfbbbc1ee267b1d403e1afe3c0b81832f
banqo <ndou92@gmail.com>
2024-03-14 22:11:03 +0200

    Register new Weather Station added

--- src/test/java/Tests/WeatherAPI/WeatherStationTests.java
+++ src/test/java/Tests/WeatherAPI/WeatherStationTests.java
@@ -1,4 +1,37 @@
 package Tests.WeatherAPI;
 
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+import static Common.CommonTestData.*;
+import static Common.RequestBuilder.*;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+@Test
+@Feature("OpenWeatherMap")
+@Story("Register new Weather Station")
 public class WeatherStationTests {
+    @Description("As an api user i want to Register a new Weather Station")
+    @Severity(SeverityLevel.CRITICAL)
+    public void registerNewWeatherStationTests() {
+        registerNewWeatherStationResponse().
+                then().
+                assertThat().
+                statusCode(Create_Success_Status_Code).
+                body("ID", notNullValue()).
+                body("updated_at", notNullValue()).
+                body("created_at", notNullValue()).
+                body("external_id", containsStringIgnoringCase("MD_Testing01")).
+                body("name", containsStringIgnoringCase("Midrand Test Station")).
+                body("latitude", notNullValue()).
+                body("longitude", notNullValue()).
+                body("altitude", notNullValue()).
+                body("rank", notNullValue()).
+                body("source_type", notNullValue());
+
+
+    }
 }
+
